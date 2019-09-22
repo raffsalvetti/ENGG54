11ceb4dc24e61050d226756f43f949d77eb509df
Lucianoal0511 <43621133+Lucianoal0511@users.noreply.github.com>
2019-09-22 16:55:58 -0300

    Sdram
    Foram feitos testes no sdram da placa TMS320C5502 eZdsp e foi identificado, que no código main.c existe uma função if para confirmar o funcionamento da placa, mas percebemos que o printf não está funcionando muito bem o que termina não trazendo confiança para a execução do programa, em virtude disso foi testada a utilização dos LEDs da placa no lugar do printf. E depois de alguns testes verificamos que o artifício funcionou com perfeição e podemos de fato fazer os testes de funcionamento da memória da placa com êxito

---
+++ manual/imagens/sdram_test.h
@@ -0,0 +1,17 @@
+/*
+ * sdram_test.h
+ *
+ *  Created on: 16 de set de 2019
+ *      Author: kau
+ */
+
+#ifndef SDRAM_TEST_H_
+#define SDRAM_TEST_H_
+
+
+Int16 MEM_addr(Uint32 start, Uint32 len);
+Int16 MEM_addrInv(Uint32 start, Uint32 len);
+Int16 sdram_test( );
+
+
+#endif /* SDRAM_TEST_H_ */
